11e2b286629c128aca296abf75da6a9441109bea
Antony Loussararian <antonyloussararian@macbookpro.home>
2025-07-10 09:31:59 +0200

    photos

--- src/data/EquipeData.ts
+++ src/data/EquipeData.ts
@@ -13,28 +13,88 @@ export const team: TeamMember[] = [
     description: "Chères Pennoises, chers Pennois, \n \n Conseiller municipal de notre commune depuis 2014 et conseiller régional de Provence-Alpes-Côte-d'Azur depuis 2021, j'ai eu l'honneur de vous représenter à différents niveaux et de porter vos préoccupations dans les instances locales et régionales. Ces deux mandats m'ont permis d'acquérir une solide expérience et une connaissance approfondie des dossiers qui concernent notre territoire. \n \n Pacsé et père de deux enfants, je suis profondément attaché à notre commune et à ce qui fait son identité : ses villages, ses traditions, son cadre de vie. Je sais à quel point il est essentiel de préserver les Pennes-Mirabeau, tout en préparant l'avenir avec responsabilité et ambition. \n \n Aujourd'hui, avec mon équipe, nous souhaitons rassembler les Pennois autour d'un projet clair, fidèle à nos valeurs et résolument tourné vers les enjeux de demain : sécurité, qualité de vie, développement durable, services publics de proximité, soutien à tous les Pennois. \n \n Je suis convaincu que c'est avec vous et pour vous que nous pourrons construire l'avenir des Pennes-Mirabeau. \n \n Avec engagement et détermination."
   },
   {
+    name: 'Emeline Coch',
+    photo: '/Emeline.png',
+    profession: 'Profession',
+    description: "Description"
+  },
+  {
+    name: 'Jean-Luc Giraud',
+    photo: '/Jean-Luc.png',
+    profession: 'Profession',
+    description: "Description"
+  },
+  {
+    name: 'Viviane Verdollin',
+    photo: '/Viviane.png',
+    profession: 'Comptable retraitée',
+    description: "Viviane, 73 ans, comptable retraitée."
+  },
+  {
     name: 'Antony Loussararian',
     photo: '/Antony.png',
     profession: 'Ingénieur Blockchain',
     description: "Antony, 23 ans, étudiant en 5e année d'ingénieurie informatique orienté blockchain, et actuellement alternant en tant qu'ingénieur blockchain."
   },
   {
-    name: 'Jean-Jacques Louedec',
-    photo: '/Jean-Jacques.png',
-    profession: 'Cuisinier des collèges à la retraite',
-    description: "Jean-Jacques, 64 ans, cusinier des collèges à la retraite."
+    name: 'Magali Kerbart',
+    photo: '/Magali.png',
+    profession: 'Chargée des ressources humaines',
+    description: "Magali, 46 ans, chargée des ressources humaines à Marseille."
   },
   {
     name: 'Maeva Trucy',
-    photo: '/default-profile.png',
+    photo: '/Maeva.png',
     profession: 'Agent polyvalent',
     description: "Maëva, 29 ans, travaille dans un collège en tant qu'agent polyvalent."
   },
   {
+    name: 'Jean-Jacques Louedec',
+    photo: '/Jean-Jacque.png',
+    profession: 'Cuisinier des collèges à la retraite',
+    description: "Jean-Jacques, 64 ans, cusinier des collèges à la retraite."
+  },
+  {
     name: 'Nadine Legal-Giavelli',
-    photo: '/default-profile.png',
+    photo: '/Nadine.png',
     profession: 'Retraité de la fonction publique territoriale',
     description: "Nadine Legal-Giavelli, 67 ans, Retraité de la fonction publique territoriale de 1982 à 2024 en tant Atsem."
+  },
+  {
+    name: 'Jérôme Vonfeld',
+    photo: '/Jerome.png',
+    profession: 'Commerçant en restauration',
+    description: "Jérôme, 47 ans, commerçant en restauration aux Pennes Mirabeau, délégué du personnel FO à Airbus."
+  },
+  {
+    name: 'Anthony Cicconardi',
+    photo: '/Anthony.png',
+    profession: 'Profession',
+    description: "Description"
+  },
+  {
+    name: 'Emma Gabriele',
+    photo: '/Emma.png',
+    profession: 'Profession',
+    description: "Description"
+  },
+  {
+    name: 'Patrice Lannez',
+    photo: '/Patrice.png',
+    profession: 'Profession',
+    description: "Description"
+  },
+  {
+    name: 'Jean-Guy Vette',
+    photo: '/Jean-Guy.png',
+    profession: 'Profession',
+    description: "Description"
+  },
+  {
+    name: 'Lyonel Joubeaux',
+    photo: '/Lyonel.png',
+    profession: 'Profession',
+    description: "Description"
   },
   {
     name: 'Vincent Ferretti',
@@ -49,69 +109,9 @@ export const team: TeamMember[] = [
     description: "Béatrice RIZZO, 66 ans, cadre A, attachée principale du ministère des armées, 46 ans d’activité."
   },
   {
-    name: 'Viviane Verdollin',
-    photo: '/default-profile.png',
-    profession: 'Comptable retraitée',
-    description: "Viviane, 73 ans, comptable retraitée."
-  },
-  {
-    name: 'Jérôme Vonfeld',
-    photo: '/default-profile.png',
-    profession: 'Commerçant en restauration',
-    description: "Jérôme, 47 ans, commerçant en restauration aux Pennes Mirabeau, délégué du personnel FO à Airbus."
-  },
-  {
-    name: 'Magali Kerbart',
-    photo: '/default-profile.png',
-    profession: 'Chargée des ressources humaines',
-    description: "Magali, 46 ans, chargée des ressources humaines à Marseille."
-  },
-  {
     name: 'Laurent Catani',
     photo: '/default-profile.png',
     profession: 'Chauffeur poids lourd',
     description: "Laurent, 33 ans, chauffeur poids lourd à son compte"
-  },
-  {
-    name: 'Anonyme',
-    photo: '/default-profile.png',
-    profession: 'Profession',
-    description: "Description du membre anonyme."
-  },
-  {
-    name: 'Anonyme',
-    photo: '/default-profile.png',
-    profession: 'Profession',
-    description: "Description du membre anonyme."
-  },
-  {
-    name: 'Anonyme',
-    photo: '/default-profile.png',
-    profession: 'Profession',
-    description: "Description du membre anonyme."
-  },
-  {
-    name: 'Anonyme',
-    photo: '/default-profile.png',
-    profession: 'Profession',
-    description: "Description du membre anonyme."
-  },
-  {
-    name: 'Anonyme',
-    photo: '/default-profile.png',
-    profession: 'Profession',
-    description: "Description du membre anonyme."
-  },
-  {
-    name: 'Anonyme',
-    photo: '/default-profile.png',
-    profession: 'Profession',
-    description: "Description du membre anonyme."
-  },
-  {
-    name: 'Anonyme',
-    photo: '/default-profile.png',
-    profession: 'Profession',
-    description: "Description du membre anonyme."
   },
 ];
