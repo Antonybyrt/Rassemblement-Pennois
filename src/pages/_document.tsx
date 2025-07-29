e72c9e8106641ab12f4a56b67b6333f8b3856b0c
Antonybyrt <antony.loussararian@gmail.com>
2025-07-29 08:58:53 +0200

    google indexation

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="fr">
+      <Head>
+        <meta name="google-site-verification" content="XQeK8JTzk7JOAghaCVZu4ukGoJ0ICJYExJ1o_dzGB6w" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
